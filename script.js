5b2737b530c126786c1309f9a5886ada900d13a7
cereia <104402370+cereia@users.noreply.github.com>
2024-02-26 13:56:55 -0500

    Add mouse hover logic
    Hovering over a square causes the color to change by adding a class name

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 //use row number to determine how many times the first row is repeated
 let gridContainer = document.querySelector('.main-container');
 
+//add mouseover event listener to add a class to whatver the mouse hovers over
+gridContainer.addEventListener('mouseover', (e) => {
+  if (e.target.classList.contains('square')) {
+    if (e.target.classList.contains('colored')) {
+      e.target.classList.remove('colored');
+    }
+    else {
+      e.target.classList.add('colored');
+    }
+  };
+})
+
 function createGrid (rows, columns) {
   //set 100 as the max number of rows or columns allowed
   if (rows > 100) rows = 100;
@@ -14,16 +26,19 @@ function createGrid (rows, columns) {
 
     //creates a full row (number of columns)
     for (let j = 0; j < columns; j++) {
-      let square = document.createElement('div');
+      let square;
+      square = document.createElement('div');
+      square.className = 'square';
 
       //to give a visual representation of the squares
       square.style = "height: 20px; width: 20px; border: 1px solid black"; 
-      square.textContent = i + 1;
 
+      //attach squares to the row div
       row.appendChild(square);
     }
+    //attach row divs to the main grid container
     gridContainer.appendChild(row);
   }
 }
 
-createGrid(16, 16)
+createGrid(20, 20);
